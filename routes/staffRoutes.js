e26b89efd3023020ed15a50df2bc6bc8e64eb542
Nutchanon28 <65010290@kmitl.ac.th>
2022-11-06 15:43:17 +0700

    fix staffController commit

--- routes/staffRoutes.js
+++ routes/staffRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const staffController = require("../controllers/StaffController");
+const staffController = require("../controllers/staffController");
 
 router
   .route("/")
